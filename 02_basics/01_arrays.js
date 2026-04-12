f1ecb1476c65514f1134c747c0c02b5cd5ce1f42
test.js <sa03065347825@gmail.com>
2026-04-12 16:50:21 +0500

    about arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,52 @@
+//  ARRAY
+
+const myArr = [1,2,3,4,5]
+// const myArr2 = new Array(1,2,3,4)
+const heros = ["Tariq pathan","Aleem shah khilji", "Imran khan"]
+
+console.log(myArr[0])
+console.log(heros[0])
+console.log(heros[0][1])
+
+//ARRAY METHOD
+
+// myArr.push(6)
+// myArr.push(0)
+// myArr.pop()   // it remove the last element by default
+
+// myArr.unshift(9)   // it add the vlue at start 
+// myArr.shift()       // it remove value from start and pop remove value from end
+
+
+// console.log(myArr.includes(5));
+// console.log(myArr.indexOf(5));
+// console.log(myArr.indexOf(20));   // if the number is not present so it eill give -1 answer
+
+
+
+
+// 
+
+// slice and splice   ( imp  interview questions)
+
+// slice: Return a copy of section af an array
+// splice: it delete element from array with last range as well (1,3) 3 will include and return the deleted element and then when we print the original array so the element deleted by splice will not be present
+// console.log("A",myArr);
+// const myn1 = myArr.slice(1,3)
+// console.log(myn1)
+// console.log(myArr);
+
+// console.log("B",myArr);
+// const myn2 = myArr.splice(1,3)
+// console.log(myn2);
+// console.log(myArr);
+
+
+// in Short : slice does not chang the original arry
+//  and splice change the original array 
+// and in splice the last index is also counted
+
+
+
+
+
